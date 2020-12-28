d2b7a725b3fc3d3c2bb00c070f2ee774c8ee58d8
Hans <romanghans@gmail.com>
2020-12-27 21:21:37 -0700

    swag sauce

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,56 @@
+
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>{{title}}</h1>
+<p>Welcome...</p>
+<teleport to=".modals" v-if="showModal">
+	<Modal :header=header text="get your ninja swag here" :theme=theme @close="toggleModal">
+		<template v-slot:links><a href="#">sign up here!</a><a href="#">more info...</a></template>
+		<h1>Giveaway!</h1>
+		<p>Get your swag here. </p>
+		
+	</Modal>
+</teleport>
+<div v-if="showModalTwo && showModal==false">
+	<Modal header="Here's Hans's header!" text="Wow, incredible!" :theme=theme @close="toggleModalTwo">
+		<template v-slot:links><a href="#">sauce up here!</a><a href="#">more inc...</a>
+		</template>
+		<h1>Here's Hans's header!</h1>
+		<p>Wow, incredible.</p>
+		
+	</Modal>
+	</div>
+<button @click="toggleModal">Open Modal</button>
+<button @click="toggleModalTwo">Open Modal Two</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components:{ Modal },
+	data(){
+		return{
+			title:"my first vue app.",
+			header:"Sign up for a free giveaway!",
+			theme:"sale",
+			showModal:false,
+			showModalTwo:false
+		}
+	},
+	methods:{
+		handleClick(){
+			console.log(this.$refs);
+			this.$refs.name.classList.add("active");
+		},
+		toggleModal(){
+			this.showModal = !this.showModal
+		},
+		toggleModalTwo(){
+			this.showModalTwo = !this.showModalTwo
+		}
+	}
 }
 </script>
 
@@ -23,4 +63,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+h1{
+	border-bottom: 1px solid #ddd;
+	display:inline-block;
+	padding-bottom: 10px;
+}
+
 </style>
